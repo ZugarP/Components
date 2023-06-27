655f124f4d888cd6940f57714bc799a2fbe27886
João Pedro Carvalho Xavier <jpcx1999@gmail.com>
2023-06-27 10:06:10 -0300

    Create GetClockInfo.tsx

---
+++ typescript/Clock/GetClockInfo.tsx
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from 'react';
+
+export const GetClockInfo = (
+	initialValue = {
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
+	},
+	getDate = () => new Date(),
+) => {
+	const [clock, setClock] = useState(() => ({
+		...initialValue,
+		isInitialized: false,
+	}))
+
+	useEffect(() => {
+		let timer: ReturnType<typeof setTimeout>
+
+		const loop = () => {
+			const now = getDate()
+			setClock({
+				hours: now.getHours(),
+				minutes: now.getMinutes(),
+				seconds: now.getSeconds(),
+				isInitialized: true,
+			})
+			const nextTick = Math.max(1, 1000 - (now.getTime() % 1000))
+			timer = setTimeout(loop, nextTick)
+		}
+		loop()
+
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [])
+
+	return clock
+
+}
